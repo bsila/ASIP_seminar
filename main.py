19822e4b2f3a3140fb4f7a0036e69c5a115daa2b
bsila <bornasila99@gmail.com>
2023-08-20 13:52:27 +0200

    Added correlation and plots

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# %% - učitavanje knjižnica i constanti
+# %% - učitavanje knjižnica i konstanti
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+import seaborn as sns
 
 months = [
     "Jan",
@@ -21,7 +21,7 @@ months = [
 data_dir = "podaci/"
 
 
-# %%
+# %% - funkcija za kreiranje niza 'čistih podataka'
 def create_water_level_array(data):
     water_level_array = []
 
@@ -34,30 +34,127 @@ def create_water_level_array(data):
                 errors="coerce",
             )
             if pd.notnull(date):
-                water_level_array.append([date, row[month]])
+                water_level_array.append([date, row[month], row["ID"]])
 
     # Sortiranje podataka
     water_level_array = sorted(water_level_array, key=lambda x: x[0])
 
     # Formatiranje datuma
     water_level_array = [
-        [date.strftime("%d.%m.%Y"), value] for date, value in water_level_array
+        [date.strftime("%d.%m.%Y"), value, id] for date, value, id in water_level_array
     ]
 
     return water_level_array
 
 
-# %% - Učitavanje i obrada podataka
+# %% - funkcija za prikaz podataka
+def plot_water_levels(water_level_array):
+    # Dinamični dohvat postaja
+    station_data = pd.DataFrame(water_level_array, columns=["Datum", "Vodostaj", "ID"])
+    unique_ids = station_data["ID"].unique()
+
+    plt.figure(figsize=(10, 6))
+
+    for station_id in unique_ids:
+        # Filtriranje podataka samo za trenutni
+        filtered_data = [row for row in water_level_array if row[2] == station_id]
+
+        # Dohvat datumo i vrijednosti
+        dates = [row[0] for row in filtered_data]
+        water_levels = [row[1] for row in filtered_data]
+
+        plt.plot(dates, water_levels, label=f"Postaja {station_id}")
+
+    plt.xlabel("Datum")
+    plt.ylabel("Vodostaj (m)")
+    plt.title("Mjereni vodostaji rijeke Albany")
+    plt.legend()
+
+    # Odabir samo ograničenog broja datuma jer se inače prikazuju svi
+    dates = station_data["Datum"].unique()
+    num_dates = len(dates)
+    max_ticks = 10  # Maksimalan broj oznaka na x-osi
+    step = max(1, num_dates // max_ticks)  # Korak za oznake na x-osi
+
+    plt.xticks(range(0, num_dates, step), dates[::step], rotation=45)
+    plt.grid(True)
+    plt.show()
+
+
+# Funkcija samo za testiranje
+def plot_water_levels_for_one_station(water_level_array, station_id):
+    # Filtriranje podataka samo za određeni ID postaje
+    filtered_data = [row for row in water_level_array if row[2] == station_id]
+
+    # Dohvat datumo i vrijednosti
+    dates = [row[0] for row in filtered_data]
+    water_levels = [row[1] for row in filtered_data]
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(dates, water_levels)
+    plt.xlabel("Datum")
+    plt.ylabel("Vodostaj (m)")
+    plt.title(f"Mjereni vodostaji postaje {station_id}")
+
+    # Odabir samo ograničenog broja datuma jer se inače prikazuju svi
+    num_dates = len(dates)
+    max_ticks = 10  # Maksimalan broj oznaka na x-osi
+    step = max(1, num_dates // max_ticks)  # Korak za oznake na x-osi
+
+    plt.xticks(range(0, num_dates, step), dates[::step], rotation=45)
+    plt.grid(True)
+    plt.show()
+
+
+# %% - funkcija za korelaciju
+def correlate_water_levels(water_level_array):
+    water_level_array = pd.DataFrame(
+        water_level_array, columns=["Datum", "Vodostaj", "ID"]
+    )
+
+    # Pretvorba stupca "Datum" u datetime format
+    water_level_array["Datum"] = pd.to_datetime(
+        water_level_array["Datum"], format="%d.%m.%Y"
+    )
+
+    # Sortiranje podataka po datumu
+    water_level_array.sort_values(by="Datum", inplace=True)
+
+    # Grupiranje podataka po ID mjerne postaje
+    grouped = water_level_array.groupby("ID")
+
+    # Izračun faznih pomaka za svaku postaju
+    fazni_pomak_po_postaji = {}
+
+    for postaja, podaci in grouped:
+        fazni_pomak = (
+            podaci["Vodostaj"].diff().fillna(0)
+        )  # Razlika između trenutnog i prethodnog vodostaja
+        fazni_pomak_po_postaji[postaja] = fazni_pomak.to_numpy()
+
+    # Pretvaranje faznih pomaka u DataFrame
+    fazni_pomak_df = pd.DataFrame(fazni_pomak_po_postaji)
+
+    # Izračun korelacijske matrice između faznih pomaka
+    correlation_matrix = fazni_pomak_df.corr()
+
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", linewidths=0.5)
+    plt.title("Korelacijska Matrica Faznih Pomaka Mjernih Postaja")
+    plt.show()
+
+
+# %% - učitavanje i obrada podataka
 # Učitavanje podataka iz datoteke
-data = pd.read_csv(f"{data_dir}daily_20230123T1539.csv", skiprows=[0])
+data_full = pd.read_csv(f"{data_dir}daily_20230123T1539.csv", skiprows=[0])
 
 # Za testiranje i provjeru podataka korišten manji dataset
 # data = pd.read_csv(f"{data_dir}test_set.csv", skiprows=[0])
 
 # Sortiranje podataka po datumu
 # !! ORIGINAL datoteka je imala ID napisan kao ' ID' - sa razmakom !!
-data.sort_values(by=["YEAR", "DD"], inplace=True)
-data = data[(data["ID"] == "04HA002") & (data["PARAM"] == 2)]
+data_full.sort_values(by=["YEAR", "DD"], inplace=True)
+data = data_full[(data_full["PARAM"] == 2)]
 
 # Odabir samo novijih podataka
 # Filtriranje podataka za postaju '04HA002', parametar 2 i godine iz 21. stoljeća
@@ -65,8 +162,13 @@ filtered_data = data[
     (data["ID"] == "04HA002") & (data["PARAM"] == 2) & (data["YEAR"] >= 2000)
 ]
 
-# Odabir samo potrebnih stupaca
+# %% - provera postaja
+unique_ids = data["ID"].unique()
+print(f"Postaje: {unique_ids}")
+
+# %% Odabir samo potrebnih stupaca
 selected_columns = [
+    "ID",
     "YEAR",
     "DD",
     "Jan",
@@ -85,34 +187,37 @@ selected_columns = [
 data = data[selected_columns]
 filtered_data = filtered_data[selected_columns]
 
-# %%
+# %% - dohvat čistih podataka
 # Za testiranje i provjeru podataka korišten manji dataset
 # water_level_array = create_water_level_array(filtered_data)
 
-# Vodostaji (PARAM == 2) su dani samo za godine 2010+
 water_level_array = create_water_level_array(data)
 
 
-# %% - Prikaz podataka
-def plot_water_levels(water_level_array):
-    dates = [row[0] for row in water_level_array]
-    water_levels = [row[1] for row in water_level_array]
+# %% - filtriranje podataka - dohvat samo oni fatuma kada postoje vodostaju za sve postaje
+# Pronađite zajedničke datume
+def get_valid_dates(water_level_array):
+    df = pd.DataFrame(water_level_array, columns=["Datum", "Vodostaj", "ID"])
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(dates, water_levels)
-    plt.xlabel("Datum")
-    plt.ylabel("Vodostaj (m)")
-    plt.title("Mjereni vodostaji rijeke Albany")
+    df = df.dropna(subset=["Vodostaj"])
 
-    # Odabir samo ograničenog broja datuma na x-osi jer se inaće prikazuju svi
-    num_dates = len(dates)
-    max_ticks = 10  # Maksimalan broj oznaka na x-osi
-    step = max(1, num_dates // max_ticks)  # Korak za oznake na x-osi
+    common_dates = set.intersection(
+        *[set(df[df["ID"] == station]["Datum"]) for station in df["ID"].unique()]
+    )
 
-    plt.xticks(range(0, num_dates, step), dates[::step], rotation=45)
-    plt.grid(True)
-    plt.show()
+    # Filtrirajte podatke samo za zajedničke datume
+    df = df[df["Datum"].isin(common_dates)]
+    water_level_array = df.values
+
+    return water_level_array
 
 
-# %% - Pozivanje funkcije za prikaz podataka
+# %% - filtriranje datuma
+water_level_array = get_valid_dates(water_level_array)
+
+# %% - pozivanje funkcije za prikaz podataka
 plot_water_levels(water_level_array)
+
+
+# %% - pozivanje funkcije za analizu
+correlate_water_levels(water_level_array)
